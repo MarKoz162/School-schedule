7f590485838f37996f536ba026aab1ad1791c7e6
MarKoz162 <88556770+MarKoz162@users.noreply.github.com>
2022-04-14 22:15:25 +0200

    Add_roles_to_users

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,23 +6,30 @@ class User < ApplicationRecord
          :confirmable, :trackable, :lockable,
          :omniauthable, omniauth_providers: [:google_oauth2, :github]
 
+  include Roleable
 
   def self.from_omniauth(access_token)
-  data = access_token.info
-  user = User.where(email: data[:email]).first
+    data = access_token.info
+    user = User.where(email: data[:email]).first
 
-  unless user
-    user = User.create(
-      email: data[:email],
-      password: Devise.friendly_token[0,20]
-    )
+    unless user
+      user = User.create(
+        email: data[:email],
+        password: Devise.friendly_token[0,20]
+      )
+    end
+    user.provider = access_token.provider
+    user.uid = access_token.uid
+    user.name = access_token.info.name
+    user.image = access_token.info.image
+    user.save
+    user.confirmed_at = Time.now
+    user
   end
-  user.provider = access_token.provider
-  user.uid = access_token.uid
-  user.name = access_token.info.name
-  user.image = access_token.info.image
-  user.save
-  user.confirmed_at = Time.now
-  user
+
+  after_create do
+    self.update(student: true)
   end
+
+
 end
