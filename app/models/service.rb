6cea2bbb56d4bb55f57ff5fa8b9991ae817dcd97
MarKoz162 <88556770+MarKoz162@users.noreply.github.com>
2022-04-20 14:40:44 +0200

    Services_validations

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,2 +1,6 @@
 class Service < ApplicationRecord
+  validates :name, presence: true, uniqueness: {case_sensitive: false}
+  validates :duration, numericality: {greater_than_or_equal_to: 0, less_than_or_equal_to: 100}
+  validates :client_price, numericality: {greater_than_or_equal_to: 0, less_than_or_equal_to: 100}
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,3 +3,12 @@ User.create(email: "example2@example.com", password: "qwe123", password_confirma
 User.create(email: "example3@example.com", password: "qwe123", password_confirmation: "qwe123" )
 
 User.update_all confirmed_at: DateTime.now
+
+
+Classroom.create(name: "Room1")
+Classroom.create(name: "Room2")
+Classroom.create(name: "Room3")
+
+Service.create(name: "Math", duration: 45 , client_price: 0)
+Service.create(name: "aaaa", duration: 45 , client_price: 0)
+Service.create(name: "dddd", duration: 45 , client_price: 0)
